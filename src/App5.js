402aaa38c1656ba64c3cfae266d2e0f4b6ba7d38
npx <npx@gmail.com>
2024-03-11 12:38:36 +0530

    Changed image to uppercase

--- src/App5.js
+++ src/App5.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function App5() {
-    //let names =["sri","chandu","madhu"];
-    let product =[{id:1, name:"product1", price:20}];
+  let products = [
+    { id: 1, name: "Product 1", price: 440, image: "/lyrosreact/images/1.PNG" },
+    { id: 2, name: "Product 2", price: 560, image: "/lyrosreact/images/2.PNG" },
+    { id: 3, name: "Product 3", price: 500, image: "/lyrosreact/images/3.PNG" },
+    { id: 4, name: "Product 4", price: 100, image: "/lyrosreact/images/4.PNG" },
+    { id: 5, name: "Product 5", price: 900, image: "/lyrosreact/images/5.PNG" },
+    { id: 6, name: "Product 6", price: 900, image: "/lyrosreact/images/6.PNG" },
+  ];
   return (
-    <ul>
-        {product.map((e,i)=>(
-        <li key={i}>{e.name}-{e.price}</li>
-        ))}
-    </ul>
-    
+    <div>
+      {products.map((e) => (
+        <div key={e.id}>
+          <div>
+            <img src={e.image} alt={e.name} />
+          </div>
+          <div>{e.name}</div>
+          <div>{e.price}</div>
+        </div>
+      ))}
+    </div>
   );
 }
-
-
